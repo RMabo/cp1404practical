ab9cbf658c94fce38ef81308fd58f9fc24b0458f
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2022-10-03 11:46:19 +1000

    Complete randoms.py as required

---
+++ Prac03/randoms.py
@@ -0,0 +1,6 @@
+"""random.py"""
+import random
+print(random.randint(5, 20))  # generating random int between the numbers 5 to 20
+print(random.randrange(3, 10, 2))  # generating random int odd numbers 3 to 10
+print(random.uniform(2.5, 5.5))  # generating random float numbers betweeen 2.5 to 5
+print(random.randint(1, 100))
