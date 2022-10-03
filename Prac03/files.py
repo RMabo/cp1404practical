a6c717c430a0e93166e7c46ae5adf1d434bc0339
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2022-10-03 11:53:09 +1000

    Complete file.py as required

--- Prac03/files.py
+++ Prac03/files.py
@@ -1,16 +1,16 @@
 """Files"""
 #1.
-# name = input("What is your name? ")
-# out_file = open("name.txt", 'r')
-# print(name, file=out_file)
-# out_file.close()
+name = input("What is your name? ")
+out_file = open("name.txt", 'r')
+print(name, file=out_file)
+out_file.close()
 
 #2
-# in_file = open("name.txt", 'r')
-# name = in_file.read()
-# name = name.strip()
-# in_file.close()
-# print("Your name is", name)
+in_file = open("name.txt", 'r')
+name = in_file.read()
+name = name.strip()
+in_file.close()
+print("Your name is", name)
 
 #3
 in_file = open("number.txt",  'r')
@@ -22,13 +22,13 @@ total = first_number + second_number
 print(total)
 
 #3 extended
-# in_file = open("number.txt",'r')
-# total = 0
-# for line in in_file:
-#     number = int(line)
-#     total += number
-# in_file.close()
-# print(total)
+in_file = open("number.txt",'r')
+total = 0
+for line in in_file:
+    number = int(line)
+    total += number
+in_file.close()
+print(total)
 
 
 
