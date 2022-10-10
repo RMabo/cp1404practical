a48999922b97449af64f8f3698dd16500599898c
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2022-10-10 13:33:22 +1000

    Complete list_warmup as required

---
+++ Prac04/lists_warmup.py
@@ -0,0 +1,28 @@
+
+# numbers[0] 3
+# numbers[-1] 2
+# numbers[3] 1
+# numbers[:-1] 3, 1, 4, 1, 5, 9
+# numbers[3:4] 1
+# 5 in numbers numbers.append(5)
+# 7 in numbers numbers.append(7)
+# "3" in numbers numbers.append('3')
+# numbers + [6, 5, 3] [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Question 1
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Question 2
+numbers[0] = 'ten'
+print(numbers)
+
+# Question 3
+numbers[-1] = 1
+print(numbers)
+
+# Question 4
+print(numbers[2:])
+print(9 in numbers)
+
+
+
