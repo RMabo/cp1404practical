c36e4a33aab759053f238f895d3ad806c25c2c64
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2022-10-13 07:30:50 +1000

    Completed list_warmup as required.

--- Prac04/lists_warmup.py
+++ Prac04/lists_warmup.py
@@ -1,28 +1,19 @@
-
-# numbers[0] 3
-# numbers[-1] 2
-# numbers[3] 1
-# numbers[:-1] 3, 1, 4, 1, 5, 9
-# numbers[3:4] 1
-# 5 in numbers numbers.append(5)
-# 7 in numbers numbers.append(7)
-# "3" in numbers numbers.append('3')
-# numbers + [6, 5, 3] [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
-
-# Question 1
+"""
+CP1404/CP5632 Practical -
+Lists "warm-up"
+"""
 numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers + [6, 5, 3])
 
-# Question 2
-numbers[0] = 'ten'
-print(numbers)
+# Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
 
-# Question 3
+
+# Change the last element of numbers to 1
 numbers[-1] = 1
-print(numbers)
-
-# Question 4
+# Print all the elements from numbers except the first two (slice)
 print(numbers[2:])
+# Print whether 9 is an element of numbers
+
 print(9 in numbers)
-
-
 
